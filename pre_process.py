6fd20c46554e60a7d7f2d08d8d3ea07ec80a1711
unknown <foamliu@yeah.net>
2019-06-23 17:58:35 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
 
     if not os.path.exists(out_path):
         os.makedirs(out_path)
-    # do_composite()
+    do_composite()
 
     # path to provided foreground images
     fg_test_path = 'data/fg_test/'
