3bb8e0d7afacb6b86c61112856baf4253dfa590a
Yang Liu <liuyang12@focusmedia.cn>
2019-07-01 14:48:35 +0800

    update

--- models.py
+++ models.py
@@ -80,7 +80,7 @@ class segnetUp1(nn.Module):
     def __init__(self, in_size, out_size):
         super(segnetUp1, self).__init__()
         self.unpool = nn.MaxUnpool2d(2, 2)
-        self.conv = conv2DBatchNormRelu(in_size, out_size, k_size=5, stride=1, padding=3, with_relu=False)
+        self.conv = conv2DBatchNormRelu(in_size, out_size, k_size=5, stride=1, padding=2, with_relu=False)
 
     def forward(self, inputs, indices, output_shape):
         outputs = self.unpool(input=inputs, indices=indices, output_size=output_shape)
