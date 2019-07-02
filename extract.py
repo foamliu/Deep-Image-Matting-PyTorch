ce9ab8bca2179d8c873b6f707af0225a7aafd3b1
Yang Liu <liuyang12@focusmedia.cn>
2019-07-02 10:08:46 +0800

    update

--- extract.py
+++ extract.py
@@ -1,15 +1,12 @@
 import zipfile
 
-
 if __name__ == '__main__':
     filename = 'data/alphamatting/input_lowres.zip'
     print('Extracting {}...'.format(filename))
-    with zipfile.open(filename) as tar:
-        tar.extractall('data/alphamatting')
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extractall('data/alphamatting')
 
     filename = 'data/alphamatting/trimap_lowres.zip'
     print('Extracting {}...'.format(filename))
-    with zipfile.open(filename) as tar:
-        tar.extractall('data/alphamatting')
-
-
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extractall('data/alphamatting')
