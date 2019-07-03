79f27fbe4fad96bef050683ab2f5b2ee6d3f62f2
Yang Liu <liuyang12@focusmedia.cn>
2019-07-03 10:08:41 +0800

    update

--- utils.py
+++ utils.py
@@ -142,10 +142,11 @@ def compute_mse_loss(y_pred, y_true):
 
 def compute_sad_loss(y_pred, y_true):
     mask = y_true[:, 1, :]
+    y_true = y_true[:, 0, :]
     y_pred = y_pred[:, 0, :]
     y_pred[mask == 1] = 1.0
     y_pred[mask == 0] = 0.0
-    diff = torch.abs(y_pred[:, 0, :] - y_true[:, 0, :])
+    diff = torch.abs(y_pred - y_true)
     return torch.sum(diff) / 1000
 
 
