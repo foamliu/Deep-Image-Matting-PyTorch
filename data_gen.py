bbfb8629476834bbef63a23645c4ab74faa35fd1
Yang Liu <liuyang12@focusmedia.cn>
2019-07-23 18:17:14 +0800

    udpate

--- data_gen.py
+++ data_gen.py
@@ -26,13 +26,13 @@ data_transforms = {
 }
 
 kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
-with open('data/Combined_Dataset/Training_set/training_fg_names.txt') as f:
+with open('Combined_Dataset/Training_set/training_fg_names.txt') as f:
     fg_files = f.read().splitlines()
-with open('data/Combined_Dataset/Training_set/training_bg_names.txt') as f:
+with open('Combined_Dataset/Training_set/training_bg_names.txt') as f:
     bg_files = f.read().splitlines()
-with open('data/Combined_Dataset/Test_set/test_fg_names.txt') as f:
+with open('Combined_Dataset/Test_set/test_fg_names.txt') as f:
     fg_test_files = f.read().splitlines()
-with open('data/Combined_Dataset/Test_set/test_bg_names.txt') as f:
+with open('Combined_Dataset/Test_set/test_bg_names.txt') as f:
     bg_test_files = f.read().splitlines()
 
 
